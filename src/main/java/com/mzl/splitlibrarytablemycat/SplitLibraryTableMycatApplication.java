a08fa5d4d8f25dbc206674a29f771e1072d1f8b3
mazhenle <2198902814@qq.com>
2023-11-01 17:45:38 +0800

    首次提交

---
+++ src/main/java/com/mzl/splitlibrarytablemycat/SplitLibraryTableMycatApplication.java
@@ -0,0 +1,13 @@
+package com.mzl.splitlibrarytablemycat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitLibraryTableMycatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplitLibraryTableMycatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mzl/splitlibrarytablemycat/SplitLibraryTableMycatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mzl.splitlibrarytablemycat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitLibraryTableMycatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
